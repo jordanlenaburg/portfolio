9af0e61ac0318d066a446b33ed598a987c82010f
Jordan Lenaburg <jordan.lenaburg@gmail.com>
2017-07-26 15:26:48 -0600

    updated contact info page

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -1,9 +1,9 @@
-var app = angular.module("mainApp")
+var app = angular.module("mainApp");
 
 app.controller("contactCtrl", ["$scope", function ($scope) {
 
     $scope.sendEmail = function (contact) {
-        emailjs.send("gmail", "contactform", contact)
+        emailjs.send("gmail", "contactform", contact);
         alert("Email Sent!")
     }
 }]);
